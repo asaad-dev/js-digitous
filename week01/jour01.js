10eac5254b7ec3f2becbf1912afdf4276823ef63
Your Name <asaadmohamed8756@gmail.com>
2021-06-14 13:27:33 +0200

    Add exos

---
+++ week01/jour01.js
@@ -0,0 +1,23 @@
+// //  01- Hello World
+// console.log("Hello World !");
+
+// //  02- String
+// var test = "My name is Asaad";
+// console.log(test);
+
+// //  03 - Concatenation
+// var name = "Asaad";
+// console.log("Nice to meet you" + " " + name);
+
+// // 04 - String Length
+// var testLength = "I'm very long !";
+// console.log(testLength.length);
+
+// // 05 - Replace 
+// var food = "croissant is meh";
+// food = food.replace("meh", "so good");
+// console.log(food);
+
+// 06- Up and Down
+var basic = "This is Cool";
+
