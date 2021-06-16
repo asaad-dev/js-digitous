338f1f5da62a4f81ecd2b792106c0771b0d27dee
Your Name <asaadmohamed8756@gmail.com>
2021-06-16 15:40:34 +0200

    Update jour03.js

--- week01/jour03.js
+++ week01/jour03.js
@@ -58,3 +58,19 @@ for (var i of sentence) {
     reversed = i + reversed;
 }
 console.log(reversed);
+
+
+//? Bonus
+for (var i = 0; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("fizzbuss");
+    } else if (i % 3 == 0) {
+        console.log("fizz");
+    } else if (i % 5 == 0) {
+        console.log("buzz");
+    } else if (i % 7 == 0) {
+        // console.log();
+    } else {
+        console.log(i);
+    }
+}
