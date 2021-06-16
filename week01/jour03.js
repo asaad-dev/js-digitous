da0c06c2fdc8e2ca639585357ec2a3a55c849d60
Your Name <asaadmohamed8756@gmail.com>
2021-06-16 15:53:28 +0200

    Update jour03.js

--- week01/jour03.js
+++ week01/jour03.js
@@ -42,35 +42,45 @@
 // // 05 Boucle
 // var total = 0;
 // var limit = 10;
-// for (var i = 0; i < limit; i++) {
+// for (var i = 1; i <= limit; i++) {
 //     console.log(i);
 // }
 
 
-// 06 - Reverse
-var sentence = "Hello Konexio";
-// for (var i = sentence.length - 1; i >= 0; i--) {
-//     console.log(sentence[i]);
+// // 06 - Reverse
+// var sentence = "Hello Konexio";
+// // for (var i = sentence.length - 1; i >= 0; i--) {
+// //     console.log(sentence[i]);
+// // }
+
+// var reversed = "";
+// for (var i of sentence) {
+//     reversed = i + reversed;
+// }
+// console.log(reversed);
+
+
+// //? Bonus
+// for (var i = 0; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("fizzbuss");
+//     } else if (i % 3 === 0) {
+//         console.log("fizz");
+//     } else if (i % 5 === 0) {
+//         console.log("buzz");
+//     } else if (i % 7 === 0) {
+//         // console.log();
+//     } else {
+//         console.log(i);
+//     }
 // }
 
-var reversed = "";
-for (var i of sentence) {
-    reversed = i + reversed;
-}
-console.log(reversed);
 
-
-//? Bonus
-for (var i = 0; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("fizzbuss");
-    } else if (i % 3 == 0) {
-        console.log("fizz");
-    } else if (i % 5 == 0) {
-        console.log("buzz");
-    } else if (i % 7 == 0) {
-        // console.log();
-    } else {
-        console.log(i);
-    }
+//? Bonus II
+var total = 0;
+var limit = 10;
+while (total <= limit) {
+    total++;
+    console.log(total);
 }
+
