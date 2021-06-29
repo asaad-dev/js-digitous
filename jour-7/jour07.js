a944c4aa300d75d60a7850c0fddc8cb41c3d4115
Your Name <asaadmohamed8756@gmail.com>
2021-06-29 08:31:05 +0200

    Update jour-7

--- jour-7/jour07.js
+++ jour-7/jour07.js
@@ -16,12 +16,13 @@ var array = [1, 2, 3, 4, 5];
 
 var double = array.map(function(num) { 
     return num * 2;
-})
+});
 
-console.log(double);
+// console.log(double);
+
 
 //  Exo : 3
-var longName = [
+var longNames = [
 	{
 		firstName: "Jane",
 		lastName: "Doe"
@@ -32,6 +33,59 @@ var longName = [
 	}
 ];
 
-var shortName = [
-    
+let shortNames = longNames.map(obj => {
+    resObj = {name: obj.firstName + " " + obj.lastName};
+    return resObj;
+});
+
+// console.log(shortNames);
+
+
+//  Exo : 04
+var array = [1, "toto", 34, "javascript", 8]
+var numbers = array.filter(function(num) {
+    return (parseInt(num));
+    // return !(parseInt(num));
+});
+
+// console.log(numbers);
+
+
+//  Exo : 05
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+var even = numbers.filter(function(numPair) {
+    return ((numPair % 2) === 0);
+})
+
+// console.log(even);
+
+
+//  Exo : 06
+var  cakes = [
+	{
+		name: "cake",
+		flavor: "vanilla",
+		status: "available"
+	},
+	{
+		name: "brownie",
+		flavor: "chocolate",
+		status: "available"
+	},
+	{
+		name: "pie",
+		flavor: "strawberry",
+		status: "available"
+	},
+	{
+		name: "muffin",
+		flavor: "pistachio",
+		status: "available"
+	},
+	{
+		name: "donut",
+		flavor: "chocolate",
+		status: "available"
+	},
 ]
