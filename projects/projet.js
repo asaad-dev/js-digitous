b14215231bd32958d033326ed23408b0fee116e5
Your Name <asaadmohamed8756@gmail.com>
2021-06-30 17:26:04 +0200

    Update projet.js

--- projects/projet.js
+++ projects/projet.js
@@ -1,3 +1,6 @@
+// Imports
+var prompt = require('prompt');
+
 var grid = [
 	["N", " ", " ", " ", " ", " ", " ", " ", " ", " "],
 	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
@@ -17,7 +20,9 @@ var rover = {
     direction: ["N", "S", "E", "W"],
     x: 0,
     y: 0,
+    travelLog: []
 };
+
 
 // 2- Functions
 
@@ -69,24 +74,76 @@ function turnRight(rover) {
   console.log(`turnRight was called!. Rover is now facing ${rover.direction}`);
 }
 
-turnRight(rover);
+// turnRight(rover);
 
 
 // Move forward function
-function moveForward() {
-
-    if(rover.direction === "N" && rover.y > 0){
-      rover.y += -1;
-    } else if(rover.direction === "E" && rover.x < 10){
-      rover.x += 1;
-    } else if(rover.direction === "S" && rover.y < 10){
-      rover.y+=1;
-    } else if(rover.direction === "W" && rover.x > 0){
-      rover.x += -1;
-    } else{
-      console.log("Position: " + rover.x + " / " + rover.y);
-    } 
-
+function moveForward(rover) {
+  if (rover.x <= 9) {
+    rover.x = rover.x + 1;
+    console.log(rover.x);
+  } else if (rover.x === 9) {
+    rover.x =rover.x;
+    console.log(rover.x);
+  } else {
+    console.log("Position " + rover.x);
   }
 
-moveForward();
+    // if(rover.direction === "N" && rover.y > 0){
+    //   rover.y += -1;
+    //   if(checkObstacle())rover.y+= 1;
+    // } else if(rover.direction === "E" && rover.x < 10){
+    //   rover.x += 1;
+    //   if(checkObstacle())rover.y+= -1;
+    // } else if(rover.direction === "S" && rover.y < 10){
+    //   rover.y+=1;
+    //   if(checkObstacle())rover.y+= -1;
+    // } else if(rover.direction === "W" && rover.x > 0){
+    //   rover.x += -1;
+    //   if(checkObstacle())rover.y+= 1;
+    // } else{
+    //   console.log("Position: " + rover.x + " / " + rover.y);
+    // } 
+
+    rover.travelLog.push("(" + rover.x + ", " + rover.y + ")");
+  }
+
+// moveForward(rover);
+
+
+// 5- Pilot rover function
+function pilotRover(str) {
+
+  prompt.start(); // Start the prompt
+
+  for(var i = 0; i < str.length; i++) {
+    if(str[i] !== "l" && str[i] !== "r" && str[i] !== "f") {
+      return "Please use only 'l', 'r', 'f'";
+    }
+  }
+
+  for (i = 0; i < str.length; i++) {
+    switch(str[i]) {
+      case "l":
+        turnLeft();
+      break;
+      
+      case "r":
+        turnRight();
+      break;
+
+      case "f":
+        moveForward();
+      break;
+    }
+  }
+
+  prompt.get(['Position :'], function (err, res) {
+
+    console.log(`Move forward : ${rover.travelLog}`);
+  })
+
+}
+
+pilotRover(rover);
+
