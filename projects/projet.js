c7cdd3a33e7cd627eaa5f0651761ee82fd7c3106
Your Name <asaadmohamed8756@gmail.com>
2021-07-01 17:24:48 +0200

    Update projet.js

--- projects/projet.js
+++ projects/projet.js
@@ -14,26 +14,25 @@ var grid = [
 	[" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 ];
 
-// console.log(grid);
+console.table(grid);
 
 var rover = {
-    direction: ["N", "S", "E", "W"],
+    direction: "N",
     x: 0,
     y: 0,
-    travelLog: []
+    travelLog: [],
 };
 
+// Obstacles 
+var obstacles = {
+  x: [1, 1, 4, 3, 2],
+  y: [0, 1, 3, 2, 9]
+};
 
 // 2- Functions
 
 // Turn left function
 function turnLeft(rover) {
-    // if (rover.direction === "S" || "E" || "W") {
-    //   console.log(`turnRight was called!. Rover is now facing ${rover.direction}`);
-    // } else {
-    //   console.log("Nope");
-    // }
-
     switch (rover.direction) {
       case "N":
         rover.direction = "E";
@@ -48,13 +47,11 @@ function turnLeft(rover) {
         rover.direction = "N";
         break;      
     } 
-    console.log(`turnLeft was called!. Rover is now facing ${rover.direction}` );
-
+    // console.log(`turnLeft was called!. Rover is now facing ${rover.direction}` );
 }
 
 turnLeft(rover); 
 
-  
 // Turn right function
 function turnRight(rover) {
   switch (rover.direction) {
@@ -71,79 +68,80 @@ function turnRight(rover) {
       rover.direction = "N";
       break;
   }
-  console.log(`turnRight was called!. Rover is now facing ${rover.direction}`);
+  // console.log(`turnRight was called!. Rover is now facing ${rover.direction}`);
 }
 
-// turnRight(rover);
-
+turnRight(rover);
 
 // Move forward function
 function moveForward(rover) {
-  if (rover.x <= 9) {
-    rover.x = rover.x + 1;
-    console.log(rover.x);
-  } else if (rover.x === 9) {
-    rover.x =rover.x;
-    console.log(rover.x);
+  // if (rover.x < 9) {
+  //   rover.x = rover.x + 1;
+  //   // console.log(rover.x);
+  // } else if (rover.x === 9) {
+  //   rover.x =rover.x;
+  //   console.log(rover.x);
+  // }
+  //   grid[rover.y][rover.x] = rover.direction;
+  //   console.table(grid);
+
+  if (rover === 'N' && rover.y > 0) {
+    rover.y += -1;
+    if(checkObstacles())rover.y += 1;
+  } else if (rover === 'E' && rover.x < 10) {
+    rover.x += 1;
+    if(checkObstacles())rover.y += -1;
+  } else if (rover === 'S' && rover.y < 10) {
+    rover.y += 1;
+    if(checkObstacles())rover.y += -1;
+  } else if (rover === 'W' && rover.x > 0) {
+    rover.x -= -1;
+    if(checkObstacles())rover.y += 1;
   } else {
-    console.log("Position " + rover.x);
+    // console.log("You must stay within the 10 X 10 grid!" + rover.x + "," + rover.y);
+  }
+  rover.travelLog.push("(" + rover.x + "," + rover.y + ")");
   }
 
-    // if(rover.direction === "N" && rover.y > 0){
-    //   rover.y += -1;
-    //   if(checkObstacle())rover.y+= 1;
-    // } else if(rover.direction === "E" && rover.x < 10){
-    //   rover.x += 1;
-    //   if(checkObstacle())rover.y+= -1;
-    // } else if(rover.direction === "S" && rover.y < 10){
-    //   rover.y+=1;
-    //   if(checkObstacle())rover.y+= -1;
-    // } else if(rover.direction === "W" && rover.x > 0){
-    //   rover.x += -1;
-    //   if(checkObstacle())rover.y+= 1;
-    // } else{
-    //   console.log("Position: " + rover.x + " / " + rover.y);
-    // } 
-
-    rover.travelLog.push("(" + rover.x + ", " + rover.y + ")");
-  }
-
-// moveForward(rover);
-
+moveForward(rover);
 
 // 5- Pilot rover function
 function pilotRover(str) {
-
-  prompt.start(); // Start the prompt
-
-  for(var i = 0; i < str.length; i++) {
-    if(str[i] !== "l" && str[i] !== "r" && str[i] !== "f") {
-      return "Please use only 'l', 'r', 'f'";
-    }
-  }
-
   for (i = 0; i < str.length; i++) {
     switch(str[i]) {
       case "l":
-        turnLeft();
+        turnLeft(rover);
       break;
-      
       case "r":
-        turnRight();
+        turnRight(rover);
       break;
-
       case "f":
-        moveForward();
+        moveForward(rover);
       break;
     }
   }
-
-  prompt.get(['Position :'], function (err, res) {
-
-    console.log(`Move forward : ${rover.travelLog}`);
-  })
-
 }
 
-pilotRover(rover);
+prompt.start(); // Start the prompt
 
+function displayRover () {
+  prompt.get(['move'], function (err, res) {
+  
+    console.log(`Position :`, rover);
+    pilotRover(res.move);
+    displayRover();
+  });
+}
+
+displayRover();
+
+// // Obstacles function
+// function checkObstacles() {
+//   for (var j = 0; j < obstacles.length; j++) {
+//     if(rover.x === obstacles.x[j] && rover.y === obstacles.y[j]) {
+//       console.log("Nope Obstacles");
+//       rturn = true;
+//     }
+//   }
+//   return false;
+// }
